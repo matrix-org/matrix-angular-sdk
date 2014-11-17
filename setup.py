1aeec80ee03460f4e4250902706d4ebaf2f6b5bb
Mark Haines <mark.haines@matrix.org>
2014-11-17 12:14:47 +0000

    Add license and readme. Add a setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+# Copyright 2014 OpenMarket Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="syweb",
+    version="0.0.1",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    description="Matrix Angular Sdk",
+    include_package_data=True,
+    long_description=read("README.rst"),
+    entry_points=""""""
+)
