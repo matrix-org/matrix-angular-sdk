a295d1c8881f382f6ccaed146b0bbbf88d8efb9f
Matthew Hodgson <matthew@matrix.org>
2014-11-18 14:54:27 -0800

    make the recents bar resizable quickly for tonight's demo

--- syweb/webclient/app/room/room-directive.js
+++ syweb/webclient/app/room/room-directive.js
@@ -272,5 +272,65 @@ angular.module('RoomController')
             });
         }
     };
-}]);
+}])
 
+// taken from http://stackoverflow.com/questions/18368485/angular-js-resizable-div-directive
+.directive('resizer', function($document) {
+
+    return function($scope, $element, $attrs) {
+
+        $element.on('mousedown', function(event) {
+            event.preventDefault();
+
+            $document.on('mousemove', mousemove);
+            $document.on('mouseup', mouseup);
+        });
+
+        function mousemove(event) {
+
+            if ($attrs.resizer == 'vertical') {
+                // Handle vertical resizer
+                var x = event.pageX - $($attrs.resizerLeft).offset().left;
+
+                if ($attrs.resizerMax && x > $attrs.resizerMax) {
+                    x = parseInt($attrs.resizerMax);
+                }
+                
+                if (x < 0) {
+                    x = 0;
+                }
+
+                $element.css({
+                    left: x + 'px'
+                });
+
+                $($attrs.resizerLeft).css({
+                    width: x + 'px'
+                });
+                $($attrs.resizerRight).css({
+                    left: (x + parseInt($attrs.resizerWidth)) + 'px'
+                });
+
+            } else {
+                // Handle horizontal resizer
+                var y = window.innerHeight - event.pageY;
+
+                $element.css({
+                    bottom: y + 'px'
+                });
+
+                $($attrs.resizerTop).css({
+                    bottom: (y + parseInt($attrs.resizerHeight)) + 'px'
+                });
+                $($attrs.resizerBottom).css({
+                    height: y + 'px'
+                });
+            }
+        }
+
+        function mouseup() {
+            $document.unbind('mousemove', mousemove);
+            $document.unbind('mouseup', mouseup);
+        }
+    };
+});
