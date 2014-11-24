249a3c7ea76db2258765cfb47bc4f7822a7c57de
Matthew Hodgson <matthew@matrix.org>
2014-11-24 00:23:10 +0000

    implement an *astoundingly* ugly hack to make the right-hand aligned bubbles line up correctly. committing to see just how badly performance breaks

--- syweb/webclient/app/room/room-directive.js
+++ syweb/webclient/app/room/room-directive.js
@@ -289,6 +289,69 @@ angular.module('RoomController')
     };
 }])
 
+// taken from http://stackoverflow.com/questions/14596213/shrink-div-to-text-thats-wrapped-to-its-max-width
+.directive('fixWidth', ['$timeout', function ($timeout) {
+    return {
+        restrict: "A",
+        scope: {
+            enabled: '=fixWidth'
+        },
+        link: function(scope, el, attrs) {
+            if (!scope.enabled) return;
+            
+            $timeout(function() {
+                // This function gets the length of some text
+                // by adding a span to the container then getting its length.
+                var getLength = function (txt) {
+                    var span = new $("<span />");
+                    if (txt == ' ')
+                        span.html('&nbsp;');
+                    else
+                        span.text(txt);
+                    el.append(span);
+                    var len = span[0].getBoundingClientRect().width;
+                    span.remove();
+                    return len;
+                };
+            
+                var words = el.text().split(' ');
+                var lengthOfSpace = getLength(' ');
+                var lengthOfLine = 0;
+                // FIXME: horrible hack to consider the grandparent rather than the span itself.
+                // This obviously shouldn't be hardcoded to the DOM structure
+                var maxElementWidth = el.parent().parent().width();
+                var maxLineLengthSoFar = 0;
+                for (var i = 0; i < words.length; i++) {
+                    // Duplicate spaces will create empty entries.
+                    if (words[i] == '')
+                        continue;
+                    // Get the length of the current word
+                    var curWord = getLength(words[i]);
+                    // Determine if adding this word to the current line will make it break
+                    if ((lengthOfLine + (i == 0 ? 0 : lengthOfSpace) + curWord) > maxElementWidth) {
+                        // If it will, see if the line we've built is the longest so far
+                        if (lengthOfLine > maxLineLengthSoFar) {
+                            //console.log("longest line so far splits before index " + i + " (word " + words[i] + " with width " + curWord + ") at width " + lengthOfLine);
+                            maxLineLengthSoFar = lengthOfLine;
+                            lengthOfLine = 0;
+                        }
+                    }
+                    else // No break yet, keep building the line
+                        lengthOfLine += (i == 0 ? 0 : lengthOfSpace) + curWord;
+                }
+                // If there are no line breaks maxLineLengthSoFar will be 0 still. 
+                // In this case we don't actually need to set the width as the container 
+                // will already be as small as possible.
+                if (maxLineLengthSoFar != 0) {
+                    // FIXME: horrible hack to consider the grandparent rather than the span itself.
+                    // This obviously shouldn't be hardcoded to the DOM structure
+                    el.parent().parent().css({ width: (maxLineLengthSoFar) + "px" });
+                }
+            });
+        }
+    }
+}])
+
 // taken from http://stackoverflow.com/questions/18368485/angular-js-resizable-div-directive
 .directive('resizer', function($document) {
 
