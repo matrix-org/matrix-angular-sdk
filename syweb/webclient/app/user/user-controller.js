7f871fd591c29af17f477e22bb888e261190ac13
Kegan Dougal <kegan@matrix.org>
2014-12-10 14:31:18 +0000

    Add user controller unit tests and rejig how user controller works. Mostly this shifting init stuff to an onInit function so it can be easily unit tested.

--- syweb/webclient/app/user/user-controller.js
+++ syweb/webclient/app/user/user-controller.js
@@ -17,47 +17,37 @@ limitations under the License.
 'use strict';
 
 angular.module('UserController', ['matrixService'])
-.controller('UserController', ['$scope', '$routeParams', 'matrixService', 'dialogService',
-                              function($scope, $routeParams, matrixService, dialogService) {                 
-    $scope.user = {
-        id: $routeParams.user_matrix_id,
-        displayname: "",
-        avatar_url: undefined
+.controller('UserController', ['$scope', '$rootScope', '$routeParams', 'matrixService', 'dialogService', 'eventHandlerService',
+                              function($scope, $rootScope, $routeParams, matrixService, dialogService, eventHandlerService) { 
+                              
+    $scope.onInit = function() {
+        $scope.user = {
+            id: $routeParams.user_matrix_id,
+            displayname: "",
+            avatar_url: undefined
+        };
+        
+        $scope.user_id = matrixService.config().user_id;
+        
+        matrixService.getDisplayName($scope.user.id).then(
+            function(response) {
+                $scope.user.displayname = response.data.displayname;
+            }
+        ); 
+        
+        matrixService.getProfilePictureUrl($scope.user.id).then(
+            function(response) {
+                $scope.user.avatar_url = response.data.avatar_url;
+            }
+        );
     };
-    
-    $scope.user_id = matrixService.config().user_id;
-    
-    matrixService.getDisplayName($scope.user.id).then(
-        function(response) {
-            $scope.user.displayname = response.data.displayname;
-        }
-    ); 
-    
-    matrixService.getProfilePictureUrl($scope.user.id).then(
-        function(response) {
-            $scope.user.avatar_url = response.data.avatar_url;
-        }
-    );
 
-    // FIXME: factor this out between user-controller and home-controller etc.
     $scope.messageUser = function() {    
-        
         // FIXME: create a new room every time, for now
-        
-        matrixService.create(null, 'private').then(
-            function(response) { 
-                // This room has been created. Refresh the rooms list
-                var room_id = response.data.room_id;
-                console.log("Created room with id: "+ room_id);
-                
-                matrixService.invite(room_id, $scope.user.id).then(
-                    function() {
-                        $scope.feedback = "Invite sent successfully";
-                        $scope.$parent.goToPage("/room/" + room_id);
-                    },
-                    function(reason) {
-                        $scope.feedback = "Failure: " + JSON.stringify(reason);
-                    });
+        eventHandlerService.createRoom(undefined, "private", [$scope.user.id]).then(
+            function(room_id) { 
+                $scope.feedback = "Invite sent successfully";
+                $rootScope.goToPage("/room/" + room_id);
             },
             function(error) {
                 dialogService.showError(error);
--- syweb/webclient/test/unit/user-controller.spec.js
+++ syweb/webclient/test/unit/user-controller.spec.js
@@ -1,57 +1,109 @@
-describe("UserCtrl", function() {
-    var scope, ctrl, matrixService, routeParams, $q, $timeout;
+describe("UserController", function() {
+    var scope, ctrl, matrixService, routeParams, $q;
     var userId = "@foo:bar";
-    var displayName = "Foo";
-    var avatarUrl = "avatar.url";
+    var targetUserId = "@target:bar";
     
-    beforeEach(module('matrixWebClient'));
-
+    var dialogService = {
+        showError: function(){}
+    };
+    
+    var matrixService = {
+        config: function(){},
+        getDisplayName: function(){},
+        getProfilePictureUrl: function(){}
+    };
+    
+    var eventHandlerService = {
+        createRoom: function(){}
+    };
+    
+    
     beforeEach(function() {
-
-        inject(function($rootScope, $injector, $controller, _$q_, _$timeout_) {
-            $q = _$q_;
-            $timeout = _$timeout_;
-
-            matrixService = {
-                config: function() {
-                    return {
-                        user_id: userId
-                    };
-                },
-
-                getDisplayName: function(uid) {
-                    var d = $q.defer();
-                    d.resolve({
-                        data: {
-                            displayname: displayName
-                        }
-                    });
-                    return d.promise;
-                },
-
-                getProfilePictureUrl: function(uid) {
-                    var d = $q.defer();
-                    d.resolve({
-                        data: {
-                            avatar_url: avatarUrl
-                        }
-                    });
-                    return d.promise;
-                }
-            };
-            scope = $rootScope.$new();
-            routeParams = {
-                user_matrix_id: userId
-            };
-            ctrl = $controller('UserController', {
-                '$scope': scope, 
-                '$routeParams': routeParams, 
-                'matrixService': matrixService
-            });
-        });
+        module('UserController');
     });
 
-    it('should display your user id', function() {
-        expect(scope.user_id).toEqual(userId);
+    beforeEach(inject(function($rootScope, $injector, $controller, _$q_) {
+            spyOn(matrixService, "config").and.returnValue({user_id:userId});
+            
+            $q = _$q_;
+            scope = $rootScope.$new();
+            rootScope = $rootScope;
+            rootScope.goToPage = function(){};
+            routeParams = {
+                user_matrix_id: targetUserId
+            };
+            ctrl = $controller('UserController', {
+                '$scope': scope,
+                '$rootScope': rootScope,
+                '$routeParams': routeParams,
+                'matrixService': matrixService,
+                'dialogService': dialogService,
+                'eventHandlerService': eventHandlerService
+            });
+        })
+    );
+
+    it('should be able to message a user', function() {
+        // setup
+        spyOn(matrixService, "getDisplayName").and.returnValue($q.defer().promise);
+        spyOn(matrixService, "getProfilePictureUrl").and.returnValue($q.defer().promise);
+        scope.onInit();
+    
+        var roomId = "!weoufhweu:matrix.org";
+        var createDefer = $q.defer();
+        spyOn(eventHandlerService, "createRoom").and.returnValue(createDefer.promise);
+        spyOn(rootScope, "goToPage");
+    
+        scope.messageUser();
+        
+        expect(eventHandlerService.createRoom).toHaveBeenCalledWith(undefined, "private", [targetUserId]);
+        createDefer.resolve(roomId);
+        scope.$digest();
+        expect(rootScope.goToPage).toHaveBeenCalledWith("/room/"+roomId);
+    });
+    
+    it('should display an error if it cannot message a user', function() {
+        // setup
+        spyOn(matrixService, "getDisplayName").and.returnValue($q.defer().promise);
+        spyOn(matrixService, "getProfilePictureUrl").and.returnValue($q.defer().promise);
+        scope.onInit();
+    
+        var roomId = "!weoufhweu:matrix.org";
+        var createDefer = $q.defer();
+        spyOn(eventHandlerService, "createRoom").and.returnValue(createDefer.promise);
+        spyOn(dialogService, "showError");
+    
+        scope.messageUser();
+        
+        expect(eventHandlerService.createRoom).toHaveBeenCalled();
+        createDefer.reject("Oh noes");
+        scope.$digest();
+        expect(dialogService.showError).toHaveBeenCalled();
+    });
+    
+    it("should request the target user's display name", function() {
+        var nameDefer = $q.defer();
+        spyOn(matrixService, "getDisplayName").and.returnValue(nameDefer.promise);
+        spyOn(matrixService, "getProfilePictureUrl").and.returnValue($q.defer().promise);
+        scope.onInit();
+        
+        var name = "Some Name";
+        expect(matrixService.getDisplayName).toHaveBeenCalledWith(targetUserId);
+        nameDefer.resolve({data:{displayname: name}});
+        scope.$digest();
+        expect(scope.user.displayname).toEqual(name);
+    });
+    
+    it("should request the target user's avatar URL", function() {
+        var picDefer = $q.defer();
+        spyOn(matrixService, "getDisplayName").and.returnValue($q.defer().promise);
+        spyOn(matrixService, "getProfilePictureUrl").and.returnValue(picDefer.promise);
+        scope.onInit();
+        
+        var pic = "http://somepic.com/pic.jpg";
+        expect(matrixService.getProfilePictureUrl).toHaveBeenCalledWith(targetUserId);
+        picDefer.resolve({data:{avatar_url:pic}});
+        scope.$digest();
+        expect(scope.user.avatar_url).toEqual(pic);
     });
 });
