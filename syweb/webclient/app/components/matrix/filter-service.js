8b9cbd86c91295e4295f26d73583fd57684afa88
Kegan Dougal <kegan@matrix.org>
2015-01-22 16:37:00 +0000

    Add filter service and unit tests.
    This manages provisioning and storage of filters, and can regenerate filters
    in the event of an expiry.

---
+++ syweb/webclient/app/components/matrix/filter-service.js
@@ -0,0 +1,181 @@
+/*
+ Copyright 2015 OpenMarket Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+'use strict';
+
+/*
+ * This service manages the provisioning and storage of filters.
+ */
+angular.module('filterService', [])
+.service('filterService', ['$q', '$window', 'matrixService', 
+function ($q, $window, matrixService) {
+    var filterService = this;
+    filterService.LS_FILTER_PREFIX = "matrix.filterService.cachedFilter_";
+
+    var Filter = function Filter() {
+        this.data = {
+            types: [],
+            not_types: [],
+            rooms: [],
+            not_rooms: [],
+            senders: [],
+            not_senders: [],
+
+            public_user_data: undefined, // true|false
+            private_user_data: undefined, // true|false
+
+            format: undefined, // normal/trimmed
+            select: [],
+
+            bundle_updates: undefined, // true|false
+            bundle_relates_to: undefined // v2.1
+        };
+
+        this.filter_token = undefined;
+    };
+
+    var removeAll = function(arr, item) {
+        var index = arr.indexOf(item);
+        while (index != -1) {
+            arr.splice(index, 1);
+            index = arr.indexOf(item);
+        }
+    };
+
+    var addIfNotExist = function(arr, item) {
+        var index = arr.indexOf(item);
+        if (index === -1) {
+            arr.push(item);
+        }
+    };
+
+    var persistFilter = function(filter) {
+        $window.localStorage.setItem(
+            filterService.LS_FILTER_PREFIX + filter.filter_token,
+            JSON.stringify(filter.data)
+        );
+    };
+
+    var getCachedFilterJson = function(filterToken) {
+        var jsonStr = $window.localStorage.getItem(
+            filterService.LS_FILTER_PREFIX + filterToken
+        );
+        if (jsonStr) {
+            return JSON.parse(jsonStr);
+        }
+    };
+
+    var provisionFilter = function(filter) {
+        var defer = $q.defer();
+
+        // strip undefined/empty array entries since they weren't used.
+        var keys = Object.keys(filter.data);
+        for (var i=0; i<keys.length; i++) {
+            var key = keys[i];
+            var val = filter.data[key];
+            if ((val === undefined) || (angular.isArray(val) && val.length == 0)) {
+                delete filter.data[key];
+            }
+        }
+
+        matrixService.createFilter(filter.data).then(function(response) {
+            filter.filter_token = response.filter_token;
+            persistFilter(filter);
+            defer.resolve(filter);
+        },
+        function(err) {
+            defer.reject(err);
+        });
+        return defer.promise;
+    };
+    
+    Filter.prototype = {
+        create: function() {
+            return provisionFilter(this);
+        },
+        includeRooms: function(roomIds) {
+            this._incl(this.data.rooms, this.data.not_rooms, roomIds);
+        },
+        excludeRooms: function(roomIds) {
+            this._excl(this.data.rooms, this.data.not_rooms, roomIds);
+        },
+        includeTypes: function(eventTypes) {
+            this._incl(this.data.types, this.data.not_types, eventTypes);
+        },
+        excludeTypes: function(eventTypes) {
+            this._excl(this.data.types, this.data.not_types, eventTypes);
+        },
+        includeSenders: function(senders) {
+            this._incl(this.data.senders, this.data.not_senders, senders);
+        },
+        excludeSenders: function(senders) {
+            this._excl(this.data.senders, this.data.not_senders, senders);
+        },
+
+        _incl: function(inclArray, exclArray, items) {
+            if(typeof(items) === 'string') {
+                items = [items];
+            }
+
+            for (var i=0; i<items.length; i++) {
+                var item = items[i];
+
+                // remove entry from excluded array
+                removeAll(exclArray, item);
+
+                // add entry into included array
+                addIfNotExist(inclArray, item);
+            }
+        },
+
+        _excl: function(inclArray, exclArray, items) {
+            if(typeof(items) === 'string') {
+                items = [items];
+            }
+
+            for (var i=0; i<items.length; i++) {
+                var item = items[i];
+
+                // remove entry from included array
+                removeAll(inclArray, item);
+
+                // add entry into excluded array
+                addIfNotExist(exclArray, item);
+            }
+        }
+    };
+
+    this.newFilter = function() {
+        return new Filter();
+    };
+
+    // regenerate a filter from the JSON (e.g. if the HS has expired it)
+    this.regenerateFilter = function(filterToken) {
+        var jsonBlob = getCachedFilterJson(filterToken);
+        if (jsonBlob) {
+            // resubmit this
+            var filter = new Filter();
+            filter.data = jsonBlob;
+            return filter.create();
+        }
+        else {
+            $q.reject("Filter not found.");
+        }
+    };
+
+}]);
+
+
---
+++ syweb/webclient/test/unit/filter-service.spec.js
@@ -0,0 +1,240 @@
+describe('FilterService', function() {
+    var $q, $rootScope;
+
+    var $window = {
+        localStorage: {
+            getItem: function(){},
+            setItem: function(){}
+        }
+    };
+
+    var matrixService = {
+        createFilter: function(){}
+    };
+
+    beforeEach(function() {
+        module(function ($provide) {
+            $provide.value('matrixService', matrixService);
+            $provide.value('$window', $window);
+        });
+        module('filterService');
+    });
+    
+    beforeEach(inject(function(_$q_, _$rootScope_) {
+        $q = _$q_;
+        $rootScope = _$rootScope_;
+    }));
+
+    it('should set Filter.filter_token from the response.', inject(
+    function(filterService) {
+        var token = "tok";
+
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.create();
+
+        defer.resolve({
+            filter_token: token
+        })
+
+        $rootScope.$digest();
+
+        expect(f.filter_token).toEqual(token);
+    }));
+
+    it('should be able to regenerate filters based on localStorage.', inject(
+    function(filterService) {
+        var token = "tok";
+        var store = {};
+        spyOn($window.localStorage, "setItem").and.callFake(function(k, v) {
+            store[k] = v;
+        });
+        spyOn($window.localStorage, "getItem").and.callFake(function(k, v) {
+            return store[k];
+        });
+
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.callFake(function() {
+            return defer.promise;
+        })
+
+        var f = filterService.newFilter();
+        f.includeRooms("!foo:bar");
+        f.create();
+
+        defer.resolve({
+            filter_token: token
+        })
+        $rootScope.$digest();
+        expect($window.localStorage.setItem).toHaveBeenCalled();
+        expect(f.filter_token).toEqual(token);
+        expect(f.data.rooms).toEqual(["!foo:bar"]);
+
+        // ... some time passes and the token is no longer valid...
+
+        // make a new promise for the new matrix API call
+        defer = $q.defer();
+        var regeneratedFilter = undefined;
+        filterService.regenerateFilter(token).then(function(filter) {
+            regeneratedFilter = filter;
+        });
+        expect($window.localStorage.getItem).toHaveBeenCalled();
+        defer.resolve({
+            filter_token: "new_tok"
+        });
+        $rootScope.$digest();
+
+        expect(regeneratedFilter.filter_token).toEqual("new_tok");
+        expect(regeneratedFilter.data.rooms).toEqual(["!foo:bar"]);
+    }));
+    
+    it('should be able to include rooms.', inject(
+    function(filterService) {
+        var roomA = "!roomA:localhost";
+        var roomB = "!roomB:localhost";
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.includeRooms([roomA, roomB]);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            rooms: [roomA, roomB]
+        });
+    }));
+
+    it('should be able to exclude rooms.', inject(
+    function(filterService) {
+        var roomA = "!roomA:localhost";
+        var roomB = "!roomB:localhost";
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.excludeRooms([roomA, roomB]);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            not_rooms: [roomA, roomB]
+        });
+    }));
+
+    it('should be able to include types.', inject(
+    function(filterService) {
+        var types = ["m.*", "org.matrix.*"];
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.includeTypes(types);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            types: types
+        });
+    }));
+
+    it('should be able to exclude types.', inject(
+    function(filterService) {
+        var types = ["m.*", "org.matrix.*"];
+        var excludeTypes = ["com.*"];
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.excludeTypes(excludeTypes);
+        f.includeTypes(types);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            types: types,
+            not_types: excludeTypes
+        });
+    }));
+
+    it('should be able to include senders.', inject(
+    function(filterService) {
+        var senders = "@bob:localhost";
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.includeSenders(senders);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            senders: [senders]
+        });
+    }));
+
+    it('should be able to exclude senders.', inject(
+    function(filterService) {
+        var senders = "@bob:localhost";
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.excludeSenders(senders);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            not_senders: [senders]
+        });
+    }));
+
+    it('should remove entries from the include list when excluding.', inject(
+    function(filterService) {
+        var roomA = "!roomA:localhost";
+        var roomB = "!roomB:localhost";
+        var roomC = "!roomC:localhost";
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.includeRooms([roomA, roomB, roomC]);
+        f.excludeRooms(roomB);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            rooms: [roomA, roomC],
+            not_rooms: [roomB]
+        });
+    }));
+
+    it('should remove entries from the exclude list when including.', inject(
+    function(filterService) {
+        var roomA = "!roomA:localhost";
+        var roomB = "!roomB:localhost";
+        var roomC = "!roomC:localhost";
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.excludeRooms([roomA, roomB, roomC, roomB, roomB]);
+        f.includeRooms(roomB);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            not_rooms: [roomA, roomC],
+            rooms: [roomB]
+        });
+    }));
+
+    it('should be able to accept a single entry as a string.', inject(
+    function(filterService) {
+        var roomA = "!roomA:localhost";
+        var defer = $q.defer();
+        spyOn(matrixService, "createFilter").and.returnValue(defer.promise);
+
+        var f = filterService.newFilter();
+        f.excludeRooms(roomA);
+        f.create();
+
+        expect(matrixService.createFilter).toHaveBeenCalledWith({
+            not_rooms: [roomA]
+        });
+    }));
+});
