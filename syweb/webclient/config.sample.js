f2aac6d4055eca91628d8e0acdbff52b52738963
Kegan Dougal <kegan@matrix.org>
2015-02-18 11:11:40 +0000

    Add sample config.js

---
+++ syweb/webclient/config.sample.js
@@ -0,0 +1,6 @@
+webClientConfig = {
+  useCaptcha: true,  // this enables the display of captchas
+  recaptcha_public_key: "1234_ABCDEF_23525_asdasd_",
+  paymentUrl: "https://some_payment_url.com/checkout",  // url to do a payment
+  paymentEulaUrl: "http://some_eula_url.com/eula"  // url to obtain the EULA
+};
