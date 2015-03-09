52887e161b93eebaeb2a07b259652350251d03ab
David Baker <dave@matrix.org>
2015-03-09 16:14:42 +0000

    Attempt to make the push rules UI suck less

--- syweb/webclient/app/settings/settings-controller.js
+++ syweb/webclient/app/settings/settings-controller.js
@@ -49,25 +49,35 @@ function($scope, matrixService, modelService, eventHandlerService, notificationS
             $scope.settings.rules = rulesets.global;
 
             var rule_descriptions = {
-                '.m.rule.contains_user_name': "Notify with sound for messages that contain my user name",
-                '.m.rule.contains_display_name': "Notify with sound for messages that contain my display name",
-                '.m.rule.room_one_to_one': "Notify with sound for messages to just me",
+                '.m.rule.contains_user_name': "Notify me with sound about messages that contain my user name",
+                '.m.rule.contains_display_name': "Notify me with sound about messages that contain my display name",
+                '.m.rule.room_one_to_one': "Notify me with sound about messages to just me",
                 '.m.rule.suppress_notices': "Suppress notifications for all automated messages",
                 '.m.rule.invite_for_me': "Notify me when I'm invited to a new room",
                 '.m.rule.member_event': "Notify me when people join or leave rooms",
-                '.m.rule.message': "Notify for messages that don't match any other rule",
+                '.m.rule.message': "Notify me about messages that don't match any other rule",
                 '.m.rule.call': "Notify me when I receive a call",
                 '.m.rule.fallback': "Notify me for anything else"
             };
+            var rule_categories = {
+                '.m.rule.suppress_notices': 'suppression',
+                '.m.rule.message': 'fallthrough',
+                '.m.rule.call': 'call',
+            };
 
-            var defaultRules = [];
+            var defaultRules = {additional: [], call: [], fallthrough: [], suppression: []};
             for (var kind in rulesets.global) {
                 for (var i = 0; i < Object.keys(rulesets.global[kind]).length; ++i) {
                     var r = rulesets.global[kind][i];
+                    var cat = rule_categories[r.rule_id];
                     r.kind = kind;
                     if (r.rule_id[0] == '.' && rule_descriptions[r.rule_id]) {
                         r.description = rule_descriptions[r.rule_id];
-                        defaultRules.push(r);
+                        if (cat) {
+                            defaultRules[cat].push(r);
+                        } else {
+                            defaultRules.additional.push(r);
+                        }
                     }
                 }
             }
