3240e2d05497f0bda5fd3b92e190eacf52b59b48
David Baker <dave@matrix.org>
2015-03-24 17:53:08 +0000

    Remove requirement for auth failures to be 403s. An unknown token would be unauthorised (401) not forbidden (403), but the Matrix spec makes no mention of HTTP status codes.

--- syweb/webclient/app/app.js
+++ syweb/webclient/app/app.js
@@ -88,7 +88,7 @@ matrixWebClient.config(['$routeProvider', '$provide', '$httpProvider',
             function ($q, $rootScope) {
             return {
                 responseError: function(rejection) {
-                    if (rejection.status === 403 && "data" in rejection && 
+                    if ("data" in rejection &&
                             "errcode" in rejection.data && 
                             rejection.data.errcode === "M_UNKNOWN_TOKEN") {
                         console.log("Got a 403 with an unknown token. Logging out.")
