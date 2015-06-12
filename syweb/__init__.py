e9c83d59fbb5b341e2126039109e306875db0490
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-06-12 18:34:28 +0100

    Add an 'installed_location()' function so syweb can report its own location

--- syweb/__init__.py
+++ syweb/__init__.py
@@ -1,4 +1,7 @@
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "webclient/VERSION")) as f:
+def installed_location():
+    return __file__
+
+with open(os.path.join(os.path.dirname(installed_location()), "webclient/VERSION")) as f:
     __version__ = f.read().strip()
