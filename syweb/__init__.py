bf51e25fdca752cac7666c351d4f0e3a00c06f52
Paul "LeoNerd" Evans <leonerd@leonerd.org.uk>
2015-06-15 12:50:22 +0100

    Have installed_location() return the dirname of __file__, not file itself, so the caller doesn't have to os.path.dirname() it manually

--- syweb/__init__.py
+++ syweb/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 def installed_location():
-    return __file__
+    return os.path.dirname(__file__)
 
-with open(os.path.join(os.path.dirname(installed_location()), "webclient/VERSION")) as f:
+with open(os.path.join(installed_location(), "webclient/VERSION")) as f:
     __version__ = f.read().strip()
